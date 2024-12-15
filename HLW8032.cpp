ec9b9690c92c92d316d2982aff5115a9d45366b0
Cullen Sharp <cullen.sharp100@gmail.com>
2024-12-14 22:45:21 -0800

    Add documentation and types

--- HLW8032.cpp
+++ HLW8032.cpp
@@ -1,35 +1,33 @@
+/**
+ * HLW8032.c - source file for energy meter library
+ * @file 			HLW8032.c
+ * @author			Habib Ullah
+ * @date			14-DEC-2024
+ * @version			1.0
+ * 
+ * Modified by Cullen Sharp 2024
+ * 
+ * This is a source file for a library to interact with a HLW8032 energy meter.
+ */
+
 #include "HLW8032.h"
 
 HLW8032::HLW8032()
-{
+{ 
+	// Construction deferred 
 }
 
-void HLW8032::begin(HardwareSerial& SerialData,byte IO)
+// Utils
+
+void HLW8032::begin(HardwareSerial& SerialData)
 {
-	 _IO = IO;
-	 pinMode(_IO,OUTPUT);
-	 digitalWrite(_IO,LOW);
-	 delay(10);
-	 SerialID = &SerialData; 
-	 SerialID->begin(4800,SERIAL_8E1);   
-	 //while(SerialID->read()>= 0){}
-	 digitalWrite(_IO,HIGH);
-	 
-	 VF = VolR1 / VolR2 ;  
-	 CF = 1.0 / (CurrentRF *1000.0);    
-}
+	
+	SerialID = &SerialData; 
+	SerialID->begin(4800,SERIAL_8E1);   
+	
 
-
-void HLW8032::setVF(float Data)
-{
-	VF = Data;
-}
-
-
-
-void HLW8032::setCF(float Data)
-{
-	CF = Data;
+	_VCoeff = (UpstrR + DwstrR) / DwstrR;  
+	_CCoeff = 1.0 / (ShuntR * 1000.0);    
 }
 
 void HLW8032::SerialReadLoop()
@@ -37,155 +35,182 @@ void HLW8032::SerialReadLoop()
 	if (SerialID->available()>0)   
 	{
 		delay(56);
-		SeriaDataLen = SerialID->available();
+
+		// Get the number of bytes available to read from serial
+		SerialDataLen = SerialID->available();
 		
-		if (SeriaDataLen !=24)
+		// A full transmission is 24 bytes
+		// From state reg to checksum reg
+		if (SerialDataLen != TRANSMISSION_LENGTH)
 		{
+			// Clear the data out
 			while(SerialID->read()>= 0){}
-			//return;
+			return;
 		}
 
-		for (byte a = 0; a < SeriaDataLen; a++)  
+		// Read bytes into an array
+		for (byte a = 0; a < SerialDataLen; a++)  
 		{
-			SerialTemps[a] =  SerialID->read();
+			SerialBuffer[a] =  SerialID->read();
 		}
-		//Serial.println(SerialID->available());
-		
 
-
-		if(SerialTemps[1] != 0x5A) 
+		// Read the Check Register
+		if(SerialBuffer[1] != 0x5A) 
 		{
 			while(SerialID->read()>= 0){}
 			return;
 		}
+
 		if(Checksum() == false)   
 		{
-			//Serial.println("crc error");
+			Serial.println("ERROR: (HLW8032): Checksum error");
 			return;
 		}
 		
+		// Raise successful read flag
+		SerialRead = 1; 
 
-		SerialRead = 1;  
-		VolPar = ((uint32_t)SerialTemps[2]  <<16) + ((uint32_t)SerialTemps[3] <<8) + SerialTemps[4]; 
-		if(bitRead(SerialTemps[20], 6) == 1)  
-		{
-			VolData = ((uint32_t)SerialTemps[5]  <<16) + ((uint32_t)SerialTemps[6] <<8) + SerialTemps[7];
-		}
-		CurrentPar = ((uint32_t)SerialTemps[8]  <<16) + ((uint32_t)SerialTemps[9] <<8) + SerialTemps[10];  
-		if(bitRead(SerialTemps[20], 5) == 1)   
-		{
-			CurrentData = ((uint32_t)SerialTemps[11]  <<16) + ((uint32_t)SerialTemps[12] <<8) + SerialTemps[13];  
-		}
-		PowerPar = ((uint32_t)SerialTemps[14]  <<16) + ((uint32_t)SerialTemps[15] <<8) + SerialTemps[16];   
-		if(bitRead(SerialTemps[20], 4) == 1)   
-		{
-			PowerData = ((uint32_t)SerialTemps[17]  <<16) + ((uint32_t)SerialTemps[18] <<8) + SerialTemps[19];    
-		}
-		PF = ((uint32_t)SerialTemps[21] <<8) + SerialTemps[22];     
+		/*
+			HLW8032 transmits data in big endian byte ordering where
+			High byte -> middle byte -> low byte
+
+			To convert to the proper value we apply LSL (i.e. multiply by 2^x)
+			High byte is multiplied by 2^16 (LSL 16)
+			Middle byte is multiplied by 2^8 (LSL 8)
+			Low byte is left unchanged
+		*/
+
+		VoltageParam = 	  ((uint32_t) SerialBuffer[2] <<16)
+						+ ((uint32_t) SerialBuffer[3] <<8)
+				 		+ SerialBuffer[4]; 
 		
+		// Check if Voltage Register is done updating
+		if(bitRead(SerialBuffer[20], 6) == 1)  
+		{
+			VoltageData = 	  ((uint32_t) SerialBuffer[5] <<16)
+							+ ((uint32_t) SerialBuffer[6] <<8)
+							+ SerialBuffer[7];
+		}
 
-		if(bitRead(SerialTemps[20], 7) == 1)
+		CurrentParam =    ((uint32_t) SerialBuffer[8] <<16)
+					 	+ ((uint32_t) SerialBuffer[9] <<8)
+						+ SerialBuffer[10];
+
+		// Check if Current Register is done updating
+		if(bitRead(SerialBuffer[20], 5) == 1)   
+		{
+			CurrentData =   ((uint32_t) SerialBuffer[11] <<16)
+						  + ((uint32_t) SerialBuffer[12] <<8)
+						  + SerialBuffer[13];  
+		}
+
+		PowerParam = 	  ((uint32_t) SerialBuffer[14] <<16)
+						+ ((uint32_t) SerialBuffer[15] <<8)
+						+ SerialBuffer[16];   
+
+		// Check if Power Register is done updating
+		if(bitRead(SerialBuffer[20], 4) == 1)   
+		{
+			PowerData = 	((uint32_t) SerialBuffer[17] <<16)
+			 			  + ((uint32_t) SerialBuffer[18] <<8)
+						  + SerialBuffer[19];    
+		}
+
+		PF = 			  ((uint16_t) SerialBuffer[21] <<8)
+						+ SerialBuffer[22];
+		
+		// PFdata refers to the number of times that PF has overflow'd
+		if(bitRead(SerialBuffer[20], 7) == 1)
 		{
 			PFData++;
 		}
 	}
 }
 
-
-
-
-float HLW8032::GetVol()
+bool HLW8032::Checksum()
 {
-	float Vol = (VolPar / VolData) * VF;   
+	byte check_sum = 0, a = 2;
+	for (;a <= TRANSMISSION_LENGTH-2; ++a) { check_sum += SerialBuffer[a]; }
+	if (check  == SerialBuffer[23]) { return true; }
+
+	return false;
+}
+
+// Setters
+
+void HLW8032::setVCoeff(float VCoeff) { _VCoeff = VCoeff; }
+
+void HLW8032::setCCoeff(float CCoeff) { _CCoeff = CCoeff; }
+
+// Getters
+
+
+voltage_t HLW8032::GetEffVoltage()
+{
+	float Vol = (VoltageParam / VoltageData) * _VCoeff;   
 	return Vol;
 } 
 
 
-float HLW8032::GetVolAnalog()
+voltage_t HLW8032::GetDividerVoltage()
 {
-	float Vol = VolPar / VolData;
-	return Vol;
+	float DividerVoltage = VoltageParam / VoltageData;
+	return DividerVoltage;
 }
 
 
-float HLW8032::GetCurrent()
+current_t HLW8032::GetEffCurrent()
 {
-	float Current = (CurrentPar / CurrentData) * CF;    
+	current_t  Current = (CurrentParam / CurrentData) * _CCoeff;    
 	return Current;
 }
 
 
-float HLW8032::GetCurrentAnalog()
+current_t HLW8032::GetShuntVoltage()
 {
-	float Current  = CurrentPar / CurrentData;
-	return Current;
+	voltage_t  ShuntVoltage  = CurrentParam / CurrentData;
+	return ShuntVoltage;
 }
 
 
 
-float HLW8032::GetActivePower()
+power_t HLW8032::GetActivePower()
 {
-	float Power = (PowerPar/PowerData) * VF * CF;  
+	power_t Power = (PowerParam/PowerData) * _VCoeff * _CCoeff;  
 	return Power;
 }
 
 
-float HLW8032::GetInspectingPower()
+power_t HLW8032::GetApparentPower()
 {
-	float vol = GetVol();
-	float current = GetCurrent();
-	return vol* current;
+	voltage_t SupplyVoltage = GetEffVoltage();
+	current_t LoadCurrent = GetEffCurrent();
+
+	return SupplyVoltage * LoadCurrent;
 }
 
 
-float HLW8032::GetPowerFactor()
+unitless_t HLW8032::GetPowerFactor()
 {
-	float ActivePower = GetActivePower();   
-	float InspectingPower = GetInspectingPower(); 
-	return ActivePower / InspectingPower ;  
+	power_t ActivePower = GetActivePower();   
+	power_t ApparentPower = GetApparentPower();
+
+	return ActivePower / ApparentPower;  
 }
 
 
+uint16_t HLW8032::GetPF() { return PF; }
 
-uint16_t HLW8032::GetPF()
+uint32_t HLW8032::GetPFAll() { return PFData * PF; }
+
+energy_t HLW8032::GetKWh()
 {
-	return PF;
-}
-
-
-
-uint32_t HLW8032::GetPFAll()
-{
-	return PFData * PF;
-}
-
-
-
-float HLW8032::GetKWh()
-{
-	float InspectingPower = GetInspectingPower();
-	uint32_t PFcnt = (1/PowerPar) *(1/InspectingPower) * 1000000000 * 3600;  
-	float KWh = (PFData * PF) / PFcnt;  
+	// Calculates the pulse count for one kWh
+	uint32_t PulseCount = 	(1/PowerParam)
+						  * (1/(_VCoeff * _CCoeff))
+						  * 1000000000 * 3600;
+	
+	energy_t KWh = (PFData * PF) / PulseCount;  
 	return KWh;
 
-}
-
-
-
-bool HLW8032::Checksum()
-{
-	byte check = 0;
-	for(byte a = 2;a<=22;a++)
-	{
-		check = check + SerialTemps[a];
-	}
-	if (check  == SerialTemps[23])
-	{
-
-		return true;
-	}
-	else
-	{
-		return false;  
-	}
 }
--- HLW8032.h
+++ HLW8032.h
@@ -1,60 +1,171 @@
+/**
+ * HLW8032.h - header file for energy meter library
+ * @file 			HLW8032.h
+ * @author			Habib Ullah
+ * @date			14-DEC-2024
+ * @version			1.0
+ * 
+ * Modified by Cullen Sharp 2024
+ * 
+ * This is a header file for interacting with a HLW8032 energy meter.
+ * 
+ * Memory map:
+ * [00,00]			State Register
+ * [01,01]			Check Register
+ * [02,04]			Voltage Parameter Register x
+ * [05,07]			Voltage Register
+ * [08,10]			Current Parameter Register
+ * [11,13]			Current Register
+ * [14,16]			Power Parameter Register
+ * [17,19]			Power Register
+ * [20,20]			Data Updata Register
+ * [21,22]			PF Register
+ * [23,23]			Checksum Register
+ */
+
 #ifndef HLW8032_h
 #define HLW8032_h
 
+// Macroes
+#define TRANSMISSION_LENGTH 24	// bytes
 
+// Types
+typedef float		voltage_t; 		// [Volt]
+typedef float		current_t; 		// [Amp]
+typedef float		resistance_t; 	// [Ohm]
+typedef float		power_t;	  	// [Watt or VA]
+typedef float		unitless_t;		// [Unitless]
+typedef float		energy_t;		// [kWh]
+
+// Check if ARDUINO IDE version is greater than 1.00
 #if ARDUINO >= 100
 #include "Arduino.h"
 #else
 #include "WProgram.h"
 #endif
 
+// Serial communication library from ARDUINO
 #include <SoftwareSerial.h>
+#include <inttypes.h>
 
 class HLW8032
 {
 	public:
+		/**
+		 * Defines a constructor (unused)
+		 */
 		HLW8032();
-		void begin(HardwareSerial& SerialData,byte IO);
-		void setVF(float Data); 
-		void setCF(float Data) ;  
-		void SerialReadLoop(); 
-		float GetVol();  
-		float GetVolAnalog();  
-		float GetCurrent(); 
-		float GetCurrentAnalog(); 
-		float GetActivePower(); 
-		float GetInspectingPower();
-		float GetPowerFactor(); 
+
+		/**
+		 * Begins serial communication at 4800 baud
+		 * And sets data, parity, and stop bits
+		 * 
+		 * @param SerialData serial communication instance
+		 */
+		void begin(HardwareSerial& SerialData);
+
+		// Set voltage coefficient for a divider net V_in/V_o = (R2 + R1)/R1
+		void setVCoeff(float VCoeff);
+
+		// Set current coefficient for shunt resistor
+		void setCCoeff(float CCoeff);  
+
+		/**
+		 * Reads 24 bytes of serial data out of the HLW8032. 
+		 * Starts with the Status REG and ends with the Checksum REG. 
+		 * Data is written to a serial buffer.
+		 * 
+		 * @note Data is read with big endian byte-ordering
+		 */
+		void SerialReadLoop();
+
+		/**
+		 * Calculates the effective voltage from power supply
+		 * @returns the effective voltage
+		 * 
+		 */
+		voltage_t GetEffVoltage();
+
+		/**
+		 * Calculates the output voltage of the divider network
+		 * @returns output voltage of the divider network
+		 */
+		voltage_t GetDividerVoltage();
+
+		/**
+		 * Calculates the effective current through a shunt resistor
+		 * @returns the effective current
+		 * 
+		 * @note This is represents the current through the load
+		 */
+		current_t GetEffCurrent(); 
+
+		/**
+		 * Calculates the voltage across a shunt resistor
+		 * @returns the voltage across a shunt resistor
+		 */
+		voltage_t GetShuntVoltage();
+
+		/**
+		 * Calculates the power dissipated in the load
+		 * @returns the power dissipated in the load
+		 * 
+		 * @note this represents the real power (i.e. power that does work)
+		 */
+		power_t GetActivePower();
+
+		/**
+		 * Calculates the apparent in the load
+		 * @returns the apparent power in the load
+		 * 
+		 * @note this represents the sum of real and reactive power
+		 */
+		power_t GetApparentPower();
+
+		/**
+		 * Calculates the % of apparent power which is real
+		 * @returns the % of apparent power which is real
+		 */
+		unitless_t GetPowerFactor();
+
+		/**
+		 * Calculates the energy consumed by a load in kWh
+		 * @returns the energy consumed by a load in kwh
+		 */
+		energy_t GetKWh();
+
 		uint16_t GetPF();
-		uint32_t GetPFAll(); 
-		float GetKWh();
-		
-		uint32_t CurrentPar; 		
-		uint32_t CurrentData; 		
-		byte SerialTemps[24] ;  
-		byte SeriaDataLen =0; 
+		uint32_t GetPFAll();
+
+			
+		byte SerialBuffer[24];  
+		byte SerialDataLen = 0; 
 		bool SerialRead = 0;  
 		
 	private:
-	
+		/**
+		 * Sums bytes 2-22 of transmision and check against the 24th (checksum)
+		 * Skips State REG (0) and Check REG (1)
+		 * 
+		 * @returns True if the sum matches the checksum
+		 */
 		bool Checksum();  
-		
-		byte _IO;
-		HardwareSerial *SerialID;
-		uint8_t SysStatus;  
-		uint32_t VolPar;   
-		uint32_t VolData;  
-		float VF;   
-		
-		float CF;            
-		uint32_t PowerPar;         
-		uint32_t PowerData;        
-		uint16_t PF;               
-		uint32_t PFData = 1;         
-		uint32_t VolR1 = 1880000;    //Voltage divider Upstream resistors 470K*4  1880K
-		uint32_t VolR2 = 1000;       //Voltage divider downstream resistors  1K
-		float CurrentRF = 0.1;     
-};
 
+		HardwareSerial *SerialID;
+		unitless_t _VCoeff;
+		unitless_t _CCoeff;
+		resistance_t UpstrR = 1880000.0;
+		resistance_t DwstrR = 1000.0;
+		resistance_t ShuntR = 0.001;
+		uint16_t PF;
+		uint32_t PFData = 1; 
+		uint32_t VoltageParam;   
+		uint32_t VoltageData;
+		uint32_t CurrentParam; 		
+		uint32_t CurrentData; 	
+		uint32_t PowerParam;         
+		uint32_t PowerData;
+        
+};
 
 #endif
